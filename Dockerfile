c3d052354bf033650e4080e30b62d4836d9f6752
Thomas GERMAIN <thgermai@e2r6p18.42.fr>
2020-01-06 18:28:03 +0100

    ca commence a ressembler a rien enfaite

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,13 @@
 FROM debian:buster
-FROM nginx
 
+RUN apt-get update
+RUN apt-get upgrade
+RUN apt-get install -y nginx
+
+COPY srcs/ /var/www/html
+
+RUN chmod +x /var/www/html/install_mysql.sh
+# RUN bash /var/www/html/install_mysql.sh
 
 EXPOSE 80
+EXPOSE 443
---
+++ srcs/install_mysql.sh
@@ -0,0 +1,6 @@
+apt-get install -y mariadb-server
+./etc/init.d/mysql start
+
+
+# execute a command int the sql terminal -> mysql --user=root --password=xxxxxx  -e "source dbscript.sql"
+# execute a script in the sql terminal -> mysql -u usr_name "db_name" < sql_script_file
